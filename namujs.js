b71dfcebb62f9469a197727bfe17fbd2a37f7fb6
JE <jeb424ds@gmail.com>
2024-09-19 18:57:42 +0900

    move D()

--- namujs.js
+++ namujs.js
@@ -16,3 +16,21 @@ function moreView(event){
         text.style.display = 'none';
       }
 }
+
+function d(){
+    const currentDate = new Date();
+    
+    // 각 구성 요소를 가져오기
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const day = currentDate.getDate();
+    const hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    
+    // 날짜와 시간을 문자열로 포맷팅
+    const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')} ${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    
+    // 포맷팅된 날짜와 시간을 출력
+    document.getElementById('d').innerText = formattedDate;
+}
